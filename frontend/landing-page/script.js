3a0e614b6bd933b4532cc18f8b07e6ea7977ec9e
Vladimir <vladimir.boussekeyt@gmail.com>
2025-09-24 14:35:26 +0200

    lien vers le wiki ajouté

---
+++ frontend/landing-page/script.js
@@ -0,0 +1,7 @@
+const wikiButton = document.getElementById("wiki")
+
+wikiButton.addEventListener('click', () => {
+    // Redirection vers une autre page
+    window.location.href = 'http://192.168.70.10:3000/';
+
+});
