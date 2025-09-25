5265a32259825e6f58fff798ba40c5de96f1dd33
gaming0307 <sulli.delettrez@gmail.com>
2025-09-25 14:19:24 +0200

    esss

--- frontend/landing-page/script.js
+++ frontend/landing-page/script.js
@@ -1,7 +1,20 @@
 const wikiButton = document.getElementById("wiki")
+const poxButton = document.getElementById("pox")
+const forumButton = document.getElementById("forum")
+
 
 wikiButton.addEventListener('click', () => {
-    // Redirection vers une autre page
     window.location.href = 'http://wiki.batman.com';
-    console.log("redirection vers le wiki")
+    console.log("redirection vers le wiki ! ")
+
 });
+poxButton.addEventListener('click', ()=>{
+
+    window.location.href = 'http://mp.batman.com';
+    console.log("redirection vers la messagerie hors ligne ! ")
+})
+forumButton.addEventListener('click', ()=>{
+
+    window.location.href = 'http://forum.batman.com';
+    console.log("redirection vers le forum ")
+})
